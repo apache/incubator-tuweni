e2388e76877e121ce744dd393a659d30190670ec
Antoine Toulme <antoine@lunar-ocean.com>
2019-05-30 15:52:53 -0700

    fix spotless

--- gossip/src/test/java/org/apache/tuweni/gossip/GossipIntegrationTest.java
+++ gossip/src/test/java/org/apache/tuweni/gossip/GossipIntegrationTest.java
@@ -12,7 +12,6 @@
  */
 package org.apache.tuweni.gossip;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
