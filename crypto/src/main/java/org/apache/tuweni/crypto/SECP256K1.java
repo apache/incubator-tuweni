e252c134a1e43b2c6baa2973db6cad4c29685f60
Antoine Toulme <antoine@lunar-ocean.com>
2019-08-25 22:25:35 -0700

    Fix source provenance and header

--- crypto/src/main/java/org/apache/tuweni/crypto/SECP256K1.java
+++ crypto/src/main/java/org/apache/tuweni/crypto/SECP256K1.java
@@ -1,14 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
- * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
- * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * Copyright 2011 Google Inc.
+ * Copyright 2014 Andreas Schildbach
+ * Copyright 2014-2016 the libsecp256k1 contributors
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.tuweni.crypto;
 
@@ -63,8 +68,6 @@ import org.bouncycastle.math.ec.custom.sec.SecP256K1Curve;
  * Adapted from the BitcoinJ ECKey (Apache 2 License) implementation:
  * https://github.com/bitcoinj/bitcoinj/blob/master/core/src/main/java/org/bitcoinj/core/ECKey.java
  *
- * Adapted from the web3j (Apache 2 License) implementations:
- * https://github.com/web3j/web3j/crypto/src/main/java/org/web3j/crypto/*.java
  */
 
 /**
