aa00d9e9a18042463702ee0af44939e6707f6d0b
Antoine Toulme <antoine@lunar-ocean.com>
2019-11-10 22:01:50 -0800

    spotless

--- devp2p/src/test/kotlin/org/apache/tuweni/devp2p/v5/storage/EnrStorageTest.kt
+++ devp2p/src/test/kotlin/org/apache/tuweni/devp2p/v5/storage/EnrStorageTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.tuweni.devp2p.v5.storage
 
 import org.apache.tuweni.crypto.Hash
@@ -14,7 +30,6 @@ class EnrStorageTest {
 
   private val storage: ENRStorage = DefaultENRStorage()
 
-
   @Test
   fun setPersistsAndFindRetrievesNodeRecord() {
     val enr = EthereumNodeRecord.toRLP(SECP256K1.KeyPair.random(), ip = InetAddress.getLocalHost())
@@ -24,5 +39,4 @@ class EnrStorageTest {
     val nodeId = Hash.sha2_256(enr)
     storage.find(nodeId)
   }
-
 }
