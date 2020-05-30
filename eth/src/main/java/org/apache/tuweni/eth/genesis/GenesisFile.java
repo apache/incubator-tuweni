eefdc8bd6db857e2c891fdd713e3fe04d9aef61f
Antoine Toulme <antoine@lunar-ocean.com>
2020-05-29 23:16:15 -0700

    Add genesis file reader

---
+++ eth/src/main/java/org/apache/tuweni/eth/genesis/GenesisFile.java
@@ -0,0 +1,247 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.tuweni.eth.genesis;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.eth.AccountState;
+import org.apache.tuweni.eth.Address;
+import org.apache.tuweni.eth.Block;
+import org.apache.tuweni.eth.BlockBody;
+import org.apache.tuweni.eth.BlockHeader;
+import org.apache.tuweni.eth.Hash;
+import org.apache.tuweni.rlp.RLP;
+import org.apache.tuweni.trie.MerklePatriciaTrie;
+import org.apache.tuweni.units.bigints.UInt256;
+import org.apache.tuweni.units.bigints.UInt64;
+import org.apache.tuweni.units.ethereum.Gas;
+import org.apache.tuweni.units.ethereum.Wei;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+/**
+ * Utility to read genesis config files and translate them to a block.
+ */
+public class GenesisFile {
+
+  private final Bytes nonce;
+  private final UInt256 difficulty;
+  private final Hash mixhash;
+  private final Address coinbase;
+  private final Instant timestamp;
+  private final Bytes extraData;
+  private final Gas gasLimit;
+  private final Map<Address, Wei> allocs;
+  private final int chainId;
+  private final List<Long> forks;
+
+
+  public GenesisFile(
+      String nonce,
+      String difficulty,
+      String mixhash,
+      String coinbase,
+      String timestamp,
+      String extraData,
+      String gasLimit,
+      String parentHash,
+      Map<String, String> allocs,
+      int chainId,
+      List<Long> forks) {
+    if (nonce == null) {
+      throw new IllegalArgumentException("nonce must be provided");
+    }
+    if (difficulty == null) {
+      throw new IllegalArgumentException("difficulty must be provided");
+    }
+    if (mixhash == null) {
+      throw new IllegalArgumentException("mixhash must be provided");
+    }
+    if (coinbase == null) {
+      throw new IllegalArgumentException("coinbase must be provided");
+    }
+    if (timestamp == null) {
+      throw new IllegalArgumentException("timestamp must be provided");
+    }
+    if (extraData == null) {
+      throw new IllegalArgumentException("extraData must be provided");
+    }
+    if (gasLimit == null) {
+      throw new IllegalArgumentException("gasLimit must be provided");
+    }
+    this.nonce = Bytes.fromHexString(nonce);
+    this.difficulty = UInt256.fromHexString(difficulty);
+    this.mixhash = Hash.fromHexString(mixhash);
+    this.coinbase = Address.fromHexString(coinbase);
+    this.timestamp = "0x0".equals(timestamp) ? Instant.ofEpochSecond(0)
+        : Instant.ofEpochSecond(Bytes.fromHexString(timestamp).toLong());
+    this.extraData = Bytes.fromHexString(extraData);
+    this.gasLimit = Gas.valueOf(Bytes.fromHexString(gasLimit).toLong());
+    this.allocs = new HashMap<>();
+    for (Map.Entry<String, String> entry : allocs.entrySet()) {
+      this.allocs.put(Address.fromHexString(entry.getKey()), Wei.valueOf(UInt256.fromHexString(entry.getValue())));
+    }
+    this.chainId = chainId;
+    this.forks = forks;
+  }
+
+  public static GenesisFile read(byte[] contents) throws IOException {
+    JsonFactory factory = new JsonFactory();
+    JsonParser parser = factory.createParser(contents);
+
+    int chainId = 0;
+    String nonce = null;
+    String difficulty = null;
+    String mixhash = null;
+    String coinbase = null;
+    String timestamp = null;
+    String extraData = null;
+    String gasLimit = null;
+    String parentHash = null;
+    Map<String, String> allocs = null;
+    List<Long> forks = new ArrayList<>();
+    while (!parser.isClosed()) {
+      JsonToken jsonToken = parser.nextToken();
+      if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+        String fieldName = parser.getCurrentName();
+
+        parser.nextToken();
+
+        if ("nonce".equals(fieldName)) {
+          nonce = parser.getValueAsString();
+        } else if ("difficulty".equals(fieldName)) {
+          difficulty = parser.getValueAsString();
+        } else if ("mixHash".equals(fieldName)) {
+          mixhash = parser.getValueAsString();
+        } else if ("coinbase".equals(fieldName)) {
+          coinbase = parser.getValueAsString();
+        } else if ("gasLimit".equals(fieldName)) {
+          gasLimit = parser.getValueAsString();
+        } else if ("timestamp".equals(fieldName)) {
+          timestamp = parser.getValueAsString();
+        } else if ("extraData".equals(fieldName)) {
+          extraData = parser.getValueAsString();
+        } else if ("parentHash".equals(fieldName)) {
+          parentHash = parser.getValueAsString();
+        } else if ("alloc".equals(fieldName)) {
+          allocs = readAlloc(parser);
+        } else if ("chainId".equals(fieldName)) {
+          chainId = parser.getValueAsInt();
+        } else if (fieldName.contains("Block")) {
+          forks.add(parser.getValueAsLong());
+        }
+      }
+
+    }
+    Collections.sort(forks);
+    return new GenesisFile(
+        nonce,
+        difficulty,
+        mixhash,
+        coinbase,
+        timestamp,
+        extraData,
+        gasLimit,
+        parentHash,
+        allocs,
+        chainId,
+        forks);
+  }
+
+  private static Map<String, String> readAlloc(JsonParser parser) throws IOException {
+    Map<String, String> allocs = new HashMap<>();
+    String name = null;
+    String value = null;
+    boolean closed = false;
+    while (!parser.isClosed()) {
+      JsonToken jsonToken = parser.nextToken();
+      if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+        closed = false;
+        String fieldName = parser.getCurrentName();
+        if ("balance".equals(fieldName)) {
+          parser.nextToken();
+          value = parser.getValueAsString();
+          allocs.put(name, value);
+        } else {
+          name = parser.getValueAsString();
+        }
+      } else if (JsonToken.END_OBJECT.equals(jsonToken)) {
+        if (closed) {
+          return allocs;
+        } else {
+          closed = true;
+        }
+      }
+    }
+    return allocs;
+  }
+
+
+  public Block toBlock() {
+    Hash emptyListHash = Hash.hash(RLP.encodeList(writer -> {
+    }));
+    Hash emptyHash = Hash.hash(RLP.encode(writer -> {
+      writer.writeValue(Bytes.EMPTY);
+    }));
+    Hash empty = Hash.hash(Bytes.EMPTY);
+    MerklePatriciaTrie<AccountState> stateTree = new MerklePatriciaTrie<>(AccountState::toBytes);
+
+    try {
+      for (Map.Entry<Address, Wei> entry : allocs.entrySet()) {
+        AccountState accountState = new AccountState(UInt256.ZERO, entry.getValue(), emptyHash, empty);
+        stateTree.putAsync(Hash.hash(entry.getKey()), accountState).join();
+      }
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    return new Block(
+        new BlockHeader(
+            null,
+            emptyListHash,
+            coinbase,
+            Hash.fromBytes(stateTree.rootHash()),
+            emptyHash,
+            emptyHash,
+            Bytes.wrap(new byte[256]),
+            difficulty,
+            UInt256.ZERO,
+            gasLimit,
+            Gas.valueOf(0L),
+            timestamp,
+            extraData,
+            mixhash,
+            UInt64.fromBytes(nonce)),
+        new BlockBody(new ArrayList<>(), new ArrayList<>()));
+  }
+
+  public Map<Address, Wei> getAllocations() {
+    return allocs;
+  }
+
+  public List<Long> getForks() {
+    return forks;
+  }
+
+  public int getChainId() {
+    return chainId;
+  }
+}
---
+++ eth/src/test/java/org/apache/tuweni/eth/genesis/GenesisFileTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.tuweni.eth.genesis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.apache.tuweni.eth.Address;
+import org.apache.tuweni.eth.Hash;
+import org.apache.tuweni.junit.BouncyCastleExtension;
+import org.apache.tuweni.units.ethereum.Wei;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BouncyCastleExtension.class)
+class GenesisFileTest {
+
+  @Test
+  void testReadGenesisFile() throws IOException {
+    InputStream input = GenesisFileTest.class.getResourceAsStream("/valid-genesis.json");
+    byte[] contents = input.readAllBytes();
+    GenesisFile file = GenesisFile.read(contents);
+    assertNotNull(file.toBlock());
+    assertEquals(0, file.toBlock().getBody().getTransactions().size());
+  }
+
+  @Test
+  void testMainnetGenesisFile() throws IOException {
+    InputStream input = GenesisFileTest.class.getResourceAsStream("/mainnet.json");
+    byte[] contents = input.readAllBytes();
+    GenesisFile file = GenesisFile.read(contents);
+    assertEquals(8893, file.getAllocations().size());
+    assertEquals(
+        Wei.valueOf(new BigInteger("327600000000000000000")),
+        file.getAllocations().get(Address.fromHexString("c951900c341abbb3bafbf7ee2029377071dbc36a")));
+    assertEquals(
+        Hash.fromHexString("0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544"),
+        file.toBlock().getHeader().getStateRoot());
+    Hash expectedHash = Hash.fromHexString("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3");
+    assertEquals(10, file.getForks().size());
+    assertEquals((Long) 1150000L, file.getForks().get(0));
+    assertEquals(expectedHash, file.toBlock().getHeader().getHash());
+  }
+
+  @Test
+  void testMissingNonce() throws IOException {
+    InputStream input = GenesisFileTest.class.getResourceAsStream("/missing-nonce.json");
+    byte[] contents = input.readAllBytes();
+    assertThrows(IllegalArgumentException.class, () -> GenesisFile.read(contents));
+  }
+
+  @Test
+  void testMissingDifficulty() throws IOException {
+    InputStream input = GenesisFileTest.class.getResourceAsStream("/missing-difficulty.json");
+    byte[] contents = input.readAllBytes();
+    assertThrows(IllegalArgumentException.class, () -> GenesisFile.read(contents));
+  }
+
+}
