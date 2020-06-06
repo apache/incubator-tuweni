0b5207c9f27957dd7b3a12fe6a9d4ec6ff92ba10
Antoine Toulme <antoine@lunar-ocean.com>
2020-06-06 16:22:58 -0700

    spotless

--- kv/src/test/java/org/apache/tuweni/kv/KeyValueStoreTest.java
+++ kv/src/test/java/org/apache/tuweni/kv/KeyValueStoreTest.java
@@ -12,11 +12,11 @@
  */
 package org.apache.tuweni.kv;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.concurrent.AsyncCompletion;
