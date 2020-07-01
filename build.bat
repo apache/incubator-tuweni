333ccbca5bc2cc080f52116abcd2d509d3a7aaa8
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-01 10:27:24 -0700

    Test sources

---
+++ build.bat
@@ -0,0 +1,15 @@
+REM Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+REM file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+REM to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+REM License. You may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+REM an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+REM specific language governing permissions and limitations under the License.
+DockerCli.exe -SwitchLinuxEngine
+cd gradle/docker
+docker build -t tuweni_test -f test.Dockerfile ../..
+cd ../..
+docker run -v %CD%/build/libs:/home/gradle/build/libs tuweni_test bash -c 'gradle -Dorg.gradle.jvmargs="-Xmx2g -XX:MaxMetaspaceSize=512m" assemble'
