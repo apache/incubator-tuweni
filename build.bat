d0fb959ec267e7f44aa382ec232a6309f6cf1980
Antoine Toulme <antoine@lunar-ocean.com>
2020-07-02 12:15:32 -0700

    try to build with gradle wrapper instead

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 REM Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
 REM file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
 REM to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
@@ -8,8 +9,8 @@ REM
 REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 REM an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 REM specific language governing permissions and limitations under the License.
-DockerCli.exe -SwitchLinuxEngine
-cd gradle/docker
-docker build -t tuweni_test -f test.Dockerfile ../..
-cd ../..
-docker run -v %CD%/build/libs:/home/gradle/build/libs tuweni_test bash -c 'gradle -Dorg.gradle.jvmargs="-Xmx2g -XX:MaxMetaspaceSize=512m" assemble'
+@echo on
+bitsadmin /transfer myDownloadJob https://services.gradle.org/distributions/gradle-6.5.1-bin.zip d:\gradle.zip
+unzip d:\gradle.zip -d gradle_download
+gradle_download/gradle-6.5.1/bin/gradle.bat setup
+./gradlew.bat assemble
