07d9dbd8a58fe13dc5cce47d85943ee47f73d3b7
Antoine Toulme <antoine@lunar-ocean.com>
2020-09-10 18:19:30 -0700

    spotless

--- devp2p/src/test/kotlin/org/apache/tuweni/devp2p/v5/topic/TicketTest.kt
+++ devp2p/src/test/kotlin/org/apache/tuweni/devp2p/v5/topic/TicketTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.tuweni.devp2p.v5.topic
 
 import org.apache.tuweni.bytes.Bytes
@@ -10,7 +26,8 @@ class TicketTest {
 
   @Test
   fun roundtrip() {
-    val ticket = Ticket(Bytes.wrap("hello world".toByteArray()), Bytes32.random(), InetAddress.getLoopbackAddress(), 0L, 0L, 0L)
+    val ticket =
+      Ticket(Bytes.wrap("hello world".toByteArray()), Bytes32.random(), InetAddress.getLoopbackAddress(), 0L, 0L, 0L)
     val key = Bytes.random(16)
     val encrypted = ticket.encrypt(key)
     assertEquals(Ticket.decrypt(encrypted, key), ticket)
