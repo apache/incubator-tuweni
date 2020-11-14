4c182ab5f544849c0c71651a9ea1b9879e6632c0
Antoine Toulme <antoine@lunar-ocean.com>
2020-11-13 21:44:26 -0800

    Add test build on Windows

--- build.bat
+++ build.bat
@@ -12,5 +12,7 @@ REM specific language governing permissions and limitations under the License.
 @echo on
 bitsadmin /transfer myDownloadJob https://services.gradle.org/distributions/gradle-6.5.1-bin.zip d:\gradle.zip
 unzip d:\gradle.zip -d gradle_download
+set "task=%1"
+if "%task%"=="" set "myvar=assemble"
 call gradle_download/gradle-6.5.1/bin/gradle.bat setup
-call gradlew.bat assemble
+call gradlew.bat %task%
