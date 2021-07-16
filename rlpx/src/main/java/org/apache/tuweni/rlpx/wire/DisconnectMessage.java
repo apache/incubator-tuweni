acf117e0801fccbdaf6b4684539282fac96a326c
Antoine Toulme <antoine@lunar-ocean.com>
2021-07-16 17:51:43 +0200

    Handle disconnects without fail. Fixes #292

--- rlpx/src/main/java/org/apache/tuweni/rlpx/wire/DisconnectMessage.java
+++ rlpx/src/main/java/org/apache/tuweni/rlpx/wire/DisconnectMessage.java
@@ -14,8 +14,28 @@ package org.apache.tuweni.rlpx.wire;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.rlp.RLP;
+import org.apache.tuweni.rlp.RLPException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class DisconnectMessage implements WireProtocolMessage {
+
+  private final static Logger logger = LoggerFactory.getLogger(DisconnectMessage.class);
+
+  static DisconnectMessage read(Bytes data) {
+    try {
+      return RLP.decode(data, (reader) -> {
+        if (reader.nextIsList()) {
+          return reader.readList((source) -> new DisconnectMessage(source.readInt()));
+        }
+        return new DisconnectMessage(DisconnectReason.NOT_PROVIDED);
+      });
+    } catch (RLPException e) {
+      logger.warn("Error reading disconnect message " + data.toHexString(), e);
+      return new DisconnectMessage(DisconnectReason.NOT_PROVIDED);
+    }
+  }
 
   private final int reason;
 
@@ -25,10 +45,6 @@ final class DisconnectMessage implements WireProtocolMessage {
 
   DisconnectMessage(int reason) {
     this.reason = reason;
-  }
-
-  static DisconnectMessage read(Bytes data) {
-    return RLP.decodeList(data, source -> new DisconnectMessage(source.readInt()));
   }
 
   @Override
