0272e8ab34b59191c0bd8e47e9ea90f792c6cc50
Antoine Toulme <antoine@lunar-ocean.com>
2021-12-16 23:56:06 -0800

    fix windows

--- build.bat
+++ build.bat
@@ -10,7 +10,4 @@ REM Unless required by applicable law or agreed to in writing, software distribu
 REM an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 REM specific language governing permissions and limitations under the License.
 @echo on
-bitsadmin /transfer myDownloadJob https://services.gradle.org/distributions/gradle-6.5.1-bin.zip d:\gradle.zip
-unzip d:\gradle.zip -d gradle_download
-call gradle_download/gradle-6.5.1/bin/gradle.bat setup
-call gradlew.bat assemble
+call ./gradlew assemble
